56c90e0febd858dca74a2d7c91ca952d3e6c3854
anyarily <michaelaybar@gmail.com>
2025-11-30 01:58:49 -0400

    modifique prueba

--- pages/componentes/Prueba.jsx
+++ pages/componentes/Prueba.jsx
@@ -4,6 +4,6 @@ export default function Prueba({date}){
     return<div>
         <button onClick={function(){ alert(date)}}></button>
 <h2>{date}</h2>
-<h2>guineo</h2>
+<h3>logica</h3>
     </div>
 }
